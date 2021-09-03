899a51dcf52db4ef734276faad050199aba5363c
Hanna <Bethany.Hanna@autofinance.com>
2021-09-02 21:30:16 -0400

    Attempted to fix the ToDo delete functionality, however it works too well and I am still unable to clear the input field.  Maybe you might have some thoughts?

--- src/Components/ToDo/TaskList.js
+++ src/Components/ToDo/TaskList.js
@@ -1,29 +1,52 @@
 import React, { Component } from 'react';
 
 export default class TaskList extends Component {
-    constructor(props){
-        super(props)
+    constructor(){
+        super()
         this.state = {
+            tasks: ["Reconcile Bank Account", "Wash Dishes", "Finish Homework"],
+            newTask: '',
             itemNumber: ''
         }
     }
 
-    markForRemoval() {
-        this.setState({toBeRemoved: true})
-        console.log(this.state.itemNumber)
+    handleSubmit() {
+        this.setState({ tasks: [...this.state.tasks, this.state.newTask]})
+        console.log(this.state.tasks);
     }
+
+    clearInput() {
+        this.setState.newTask = ''
+    }
+
+    markComplete() {
+        console.log(this.state.itemNumber)
+        this.state.tasks.splice(this.state.itemNumber, 1, )
+    }    
 
     render() {
         return(
             <>
-                {this.props.taskList.map((t, index) => {
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                this.handleSubmit()
+                this.clearInput()
+                }}>
+                <label htmlFor="newTask"><b>Add A New Task:</b></label>
+                <br />
+                <input placeholder="New Task" name="newTask" type="text" maxLength="200" onChange={(e) => {this.setState({newTask: e.target.value})}} />
+                <br />
+                <br />
+                <button type="submit">Add My Task</button>
+            </form>
+                {this.state.tasks.map((t, index) => {
                     return(
-                        <p>
+                        <p key={index}>
                         <label htmlFor="checkbox">
                             {t}&nbsp;
-                            <input name="checkbox" value={index} type="checkbox" onClick={(e) => {
-                                this.setState({ itemNumber: e.currentTarget.value})
-                                this.markForRemoval()
+                            <input name="checkbox" value={index} type="checkbox" onChange={(e) => {
+                                this.setState({ itemNumber: e.currentTarget.index})
+                                this.markComplete(this.state.itemNumber)
                             }} />
                         </label>
                         <br />
--- src/Components/ToDo/ToDo.js
+++ src/Components/ToDo/ToDo.js
@@ -3,69 +3,35 @@ import {Container} from 'reactstrap';
 import TaskList from './TaskList';
 
 class ToDo extends Component {
-    constructor(){
-      super()
-      this.state = {
-        tasks: ["Reconcile Bank Account", "Wash Dishes", "Finish Homework"],
-        newTask: '',
-        toBeRemoved: false,
-        itemNumber: ''
-      }
-    }
-
-    handleSubmit() {
-      this.setState({ tasks: [...this.state.tasks, this.state.newTask]})
-      console.log(this.state.tasks);
-    }
-
-    clearInput() {
-      this.setState.newTask = ''
-    }
-
-    markComplete() {
-      this.state.tasks.splice(this.state.itemNumber, 1, )
-    }
-  
+      
   render() {
     return(
       <Container className="App">
-        <form onSubmit={(e) => {
-          e.preventDefault()
-          this.handleSubmit()
-          this.clearInput()
-        }}>
-          <label htmlFor="newTask"><b>Add A New Task:</b></label>
-          <br />
-          <input placeholder="New Task" name="newTask" type="text" maxLength="200" onChange={(e) => {this.setState({newTask: e.target.value})}} />
-          <br />
-          <br />
-          <button type="submit">Add My Task</button>
-        </form>
         <br />
-        <TaskList taskList={this.state.tasks} toBeRemoved={this.state.toBeRemoved} itemNumber={this.state.itemNumber} />
+        <TaskList />
       <br />
         <h1 className="App-intro">
           In this challenge your goal is to create a to do list: 
         </h1>
         <hr />
-        <ul>
+        {/* <ul>
           <li>
             ability to accept a task from a user
           </li>
           {/* Will need an input field and button to add new task to state */}
-          <li>
+          {/* <li>
               ability to add this to the state
-          </li>
+          </li> */}
           {/* Will want to add to state as an array of tasks so +e.target.value is the correct way I believe */}
-          <li>
+          {/* <li>
               ability to display all of the tasks
-          </li>
+          </li> */}
           {/* Review the putting-it-together for some pointers (just remember that is typescript! not everything will work here) */}
-          <li>
+          {/* <li>
               ability to evaluated if a task was completed and update the state
-          </li>
+          </li> */}
           {/* Not sure what they are asking here...maybe a checkbox or something? Maybe another button to mark 'done'? */}
-        </ul>
+        {/* </ul> */}
         <hr />
       </Container>
     );
